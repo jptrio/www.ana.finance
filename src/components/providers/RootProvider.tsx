52ca29e0c954c992497bac1d22fc27221396bce0
Negative <113067475+negative-tnc@users.noreply.github.com>
2023-03-06 19:27:20 -0600

    fix: remove next auth for now

--- src/components/providers/RootProvider.tsx
+++ src/components/providers/RootProvider.tsx
@@ -1,18 +1,18 @@
 import Web3Provider from '@/components/providers/Web3Provider'
 import theme from '@/config/theme'
 import { ChakraProvider } from '@chakra-ui/react'
-import { SessionProvider } from 'next-auth/react'
+// import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
 import { SWRConfig } from 'swr'
 
 export default function RootProvider({ children }: { children: ReactNode }) {
   return (
     <SWRConfig>
-      <SessionProvider>
+      {/*<SessionProvider>*/}
         <ChakraProvider theme={theme}>
           <Web3Provider>{children}</Web3Provider>
         </ChakraProvider>
-      </SessionProvider>
+      {/*</SessionProvider>*/}
     </SWRConfig>
   )
 }
